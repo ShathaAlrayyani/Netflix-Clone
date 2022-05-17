2cae9bf1111a56cb1f0ac03cb6993de2c2d5dfbb
Shatha Alrayyani <shatha.alrayyani223@gmail.com>
2022-05-17 05:47:58 +0300

    task18

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Components/Home/home';
+import { Routes, Route } from "react-router-dom";
+//import NavBar from "./components/NavBar";
+import FavRecipe from "./components/FavRecipe";
+
+
+function App() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/home.js
@@ -0,0 +1,13 @@
+import NavBar from "../Navbar/navBar"
+let url = "https://movies-library15.herokuapp.com/trending"
+
+
+export default function Home (){
+
+    return(
+        <>
+        <NavBar/>
+
+        </>
+    )
+}
